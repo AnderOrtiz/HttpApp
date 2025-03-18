83b823786fdd2791199dc9381606ebf29b8b651d
Anderson Ortiz <ortizone620@gmail.com>
2025-03-17 22:47:42 -0600

    Next Quote

--- src/breakingbad-app/breakingbad-app.js
+++ src/breakingbad-app/breakingbad-app.js
@@ -1,10 +1,48 @@
 
+/**
+ * @returns {Object}
+ */
+const fetchQuote = async() => {
+
+    const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes/')
+    const data = await res.json();
+
+    console.log(data[0]);
+    return data[0];
+}
 
 /**
  * 
  * @param {HTMLElement} element 
  */
-export const BreakingbadApp= (element) =>{
+export const BreakingbadApp= async(element) =>{
+
     document.querySelector('#app-title').innerHTML = 'Breakingbad App'
     element.innerHTML = 'Loanding...'
+
+    // const quote = await fetchQuote();
+
+    const quoteLabel = document.createElement('blockquote');
+    const authoLabel = document.createElement('h3');
+    const nextQuoteButton = document.createElement('button');
+    nextQuoteButton.innerText = 'Next Quote';
+
+
+    const renderQuote = (data) => {
+        quoteLabel.innerHTML = data.quote;
+        authoLabel.innerHTML = data.author;
+        element.replaceChildren(quoteLabel, authoLabel, nextQuoteButton);
+    }
+
+
+    nextQuoteButton.addEventListener('click', async() =>{
+        element.innerHTML = 'Loanding...'
+        const quote = await fetchQuote()
+        renderQuote(quote);
+    })
+
+
+    fetchQuote()
+        .then(renderQuote);
+
 }
